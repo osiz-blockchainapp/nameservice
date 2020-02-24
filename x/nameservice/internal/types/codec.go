126ddf911dd1da410e41043f81cad27ee303f142
sultana <sultanaosiz@bitzbillo.com>
2020-02-24 15:06:19 +0530

    apptesting

---
+++ x/nameservice/internal/types/codec.go
@@ -0,0 +1,27 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+// ModuleCdc is the codec for the module
+var ModuleCdc = codec.New()
+
+func init() {
+	RegisterCodec(ModuleCdc)
+}
+
+// RegisterCodec registers concrete types on the Amino codec
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgSetName{}, "nameservice/SetName", nil)
+	cdc.RegisterConcrete(MsgBuyName{}, "nameservice/BuyName", nil)
+	cdc.RegisterConcrete(MsgDeleteName{}, "nameservice/DeleteName", nil)
+}
+// MakeCodec generates the necessary codecs for Amino
+func MakeCodec() *codec.Codec {
+	var cdc = codec.New()
+	ModuleBasics.RegisterCodec(cdc)
+	sdk.RegisterCodec(cdc)
+	codec.RegisterCrypto(cdc)
+	return cdc
+}
